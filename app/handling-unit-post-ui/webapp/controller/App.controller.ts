7dc61cf8fc4b45c2e6216546bd8de37f9ea424fa
Gizem Nur Soylu <gizemnur.soylu@nttdata.com>
2024-07-09 10:16:27 +0000

    ui module added

---
+++ app/handling-unit-post-ui/webapp/controller/App.controller.ts
@@ -0,0 +1,14 @@
+import Controller from "sap/ui/core/mvc/Controller";
+
+/**
+ * @namespace com.ndbs.handlingunitpostui.controller
+ */
+export default class App extends Controller {
+    /* ======================================================================================================================= */
+    /* Lifecycle methods                                                                                                       */
+    /* ======================================================================================================================= */
+
+    public onInit(): void {
+     
+    }
+}

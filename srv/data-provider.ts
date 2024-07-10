f2d77de9431e7035a1e0bcffb5046ec31a2fdb69
gizemsoylu <gizemnur.soylu@nttdata.com>
2024-07-10 14:25:27 +0300

    first deploy config

---
+++ srv/data-provider.ts
@@ -0,0 +1,8 @@
+import { ApplicationService } from "@sap/cds";
+
+export default class IssMassHire extends ApplicationService {
+    async init(): Promise<void> {
+        
+        return super.init();
+    }
+}

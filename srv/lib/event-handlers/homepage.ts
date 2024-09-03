bbe5d4d31f4ba6f8a4d949b8c22f9c8fddf9db6b
gizemsoylu <gizemnur.soylu@nttdata.com>
2024-09-04 00:13:26 +0300

    productionOrder field added instead of ProductionOrder_1

--- srv/lib/event-handlers/homepage.ts
+++ srv/lib/event-handlers/homepage.ts
@@ -187,12 +187,12 @@ const getStorageTypes: OnEventHandler = async function (req: TypedRequest<{ EWMS
 
 const getProductionOrders: OnEventHandler = async function (req: TypedRequest<{ ProductionOrder: string }[]>): Promise<{ ProductionOrder: string }[]> {
     const handlingCDS = await connect.to("YY1_HUINFOPALLETBOX");
-    const allOrders = await handlingCDS.run(SELECT.from('YY1_HUInfoPalletbox_ewm').columns('ProductionOrder_1'));
+    const allOrders = await handlingCDS.run(SELECT.from('YY1_HUInfoPalletbox_ewm').columns('ProductionOrder'));
     let uniqueOrders: { ProductionOrder: string }[] = [];
 
-    allOrders.forEach((item: { ProductionOrder_1: string; }) => {
-        if (item.ProductionOrder_1 !== '' && !uniqueOrders.some(warehouse => warehouse.ProductionOrder === item.ProductionOrder_1)) {
-            uniqueOrders.push({ ProductionOrder: item.ProductionOrder_1 });
+    allOrders.forEach((item: { ProductionOrder: string; }) => {
+        if (item.ProductionOrder !== '' && !uniqueOrders.some(warehouse => warehouse.ProductionOrder === item.ProductionOrder)) {
+            uniqueOrders.push({ ProductionOrder: item.ProductionOrder });
         }
     });
 

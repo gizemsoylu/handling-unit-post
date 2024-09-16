7ab222515d0d97ab62f3b02f1d131cc0e42ae22f
gizemsoylu <gizemnur.soylu@nttdata.com>
2024-09-17 01:31:28 +0300

    search operation added all filters

--- srv/lib/event-handlers/homepage.ts
+++ srv/lib/event-handlers/homepage.ts
@@ -158,12 +158,12 @@ const getHandlingUnitNumbers: OnEventHandler = async function (req: TypedRequest
             uniqueHUs = uniqueHUs.filter((hu: { HUNumber: string }) =>
                 searchValues.some(searchValue => hu.HUNumber.includes(searchValue))
             );
-        } else {
-            // Eğer search parametresi yoksa, HUNumber'ları sadece formatla
-            uniqueHUs = uniqueHUs.map(hu => ({ HUNumber: hu.HUNumber }));
         }
 
-        return uniqueHUs;
+        const finalUniqueHUs = Array.from(new Set(uniqueHUs.map(hu => hu.HUNumber)))
+            .map(hu => ({ HUNumber: hu }));
+
+        return finalUniqueHUs;
 
     } catch (error) {
         console.error("Error in getHandlingUnitNumbers: ", error);
@@ -171,88 +171,193 @@ const getHandlingUnitNumbers: OnEventHandler = async function (req: TypedRequest
     }
 };
 
+
 const getProducts: OnEventHandler = async function (req: TypedRequest<{ Product: string }[]>): Promise<{ Product: string }[]> {
-    const handlingCDS = await connect.to("YY1_HUINFOPALLETBOX");
-    const allProduct = await handlingCDS.run(SELECT.from('YY1_HUInfoPalletbox_ewm').columns('Product'));
-    let uniqueProduct: { Product: string }[] = [];
+    try {
+        const handlingCDS = await connect.to("YY1_HUINFOPALLETBOX");
+        const allProduct = await handlingCDS.run(SELECT.from('YY1_HUInfoPalletbox_ewm').columns('Product'));
+        let uniqueProduct: { Product: string }[] = [];
 
-    allProduct.forEach((item: { Product: string; }) => {
-        if (item.Product !== '' && !uniqueProduct.some(warehouse => warehouse.Product === item.Product)) {
-            uniqueProduct.push({ Product: item.Product });
+        allProduct.forEach((item: { Product: string }) => {
+            if (item.Product !== '' && !uniqueProduct.some(product => product.Product === item.Product)) {
+                uniqueProduct.push({ Product: item.Product });
+            }
+        });
+
+        if ((req.query.SELECT as any)?.search) {
+            const filters = (req.query.SELECT as any).search as IWhereClause[];
+            const searchValues = filters.map(filter => filter.val as string);
+
+            uniqueProduct = uniqueProduct.filter((product: { Product: string }) =>
+                searchValues.some(searchValue => product.Product.includes(searchValue))
+            );
         }
-    });
 
-    return uniqueProduct;
-}
+        const finalUniqueProducts = Array.from(new Set(uniqueProduct.map(product => product.Product)))
+            .map(product => ({ Product: product }));
 
-const getVHStorageBins: OnEventHandler = async function (req: TypedRequest<{ EWMStorageBin_1: string }[]>): Promise<{ EWMStorageBin: string }[]> {
-    const handlingCDS = await connect.to("YY1_HUINFOPALLETBOX");
-    const allStorageBins = await handlingCDS.run(SELECT.from('YY1_HUInfoPalletbox_ewm').columns('EWMStorageBin_1'));
-    let uniqueStorageBins: { EWMStorageBin: string }[] = [];
+        return finalUniqueProducts;
 
-    allStorageBins.forEach((item: { EWMStorageBin_1: string; }) => {
-        if (item.EWMStorageBin_1 !== '' && !uniqueStorageBins.some(warehouse => warehouse.EWMStorageBin === item.EWMStorageBin_1)) {
-            uniqueStorageBins.push({ EWMStorageBin: item.EWMStorageBin_1 });
-        }
-    });
-
-    return uniqueStorageBins;
-}
-
-const getStorageTypes: OnEventHandler = async function (req: TypedRequest<{ EWMStorageType_1: string }[]>): Promise<{ EWMStorageType: string }[]> {
-    const handlingCDS = await connect.to("YY1_HUINFOPALLETBOX");
-    const allStorageTypes = await handlingCDS.run(SELECT.from('YY1_HUInfoPalletbox_ewm').columns('EWMStorageType_1').where({
-        PackagingMaterialType: ['Z001', 'Z002']
-    }));
-    let uniqueStorageTypes: { EWMStorageType: string }[] = [];
-
-    allStorageTypes.forEach((item: { EWMStorageType_1: string; }) => {
-        if (item.EWMStorageType_1 !== '' && !uniqueStorageTypes.some(warehouse => warehouse.EWMStorageType === item.EWMStorageType_1)) {
-            uniqueStorageTypes.push({ EWMStorageType: item.EWMStorageType_1 });
-        }
-    });
-
-    return uniqueStorageTypes;
-}
-
-const getProductionOrders: OnEventHandler = async function (req: TypedRequest<{ ProductionOrder: string }[]>): Promise<{ ProductionOrder: string }[]> {
-    const handlingCDS = await connect.to("YY1_HUINFOPALLETBOX");
-    const allOrders = await handlingCDS.run(SELECT.from('YY1_HUInfoPalletbox_ewm').columns('ProductionOrder'));
-    let uniqueOrders: { ProductionOrder: string }[] = [];
-
-    allOrders.forEach((item: { ProductionOrder: string; }) => {
-        if (item.ProductionOrder !== '' && !uniqueOrders.some(warehouse => warehouse.ProductionOrder === item.ProductionOrder)) {
-            uniqueOrders.push({ ProductionOrder: item.ProductionOrder });
-        }
-    });
-
-    return uniqueOrders;
-}
-
-const getEWMWarehouseBins: OnEventHandler = async function (req: TypedRequest<{ EWMWarehouse: string }>): Promise<IStorageBins[]> {
-    const EWMWarehouse = req.data.EWMWarehouse;
-    const storageService = await connect.to("WAREHOUSESTORAGEBIN");
-    const { WarehouseStorageBin } = storageService.entities;
-    let storageBins: [IStorageBins];
-    storageBins = await storageService.run(SELECT.from(WarehouseStorageBin).where({ EWMWarehouse: EWMWarehouse }));
-    return storageBins;
-}
-
-const getStorageBins: OnEventHandler = async function (req: TypedRequest<IStorageBins[]>): Promise<IStorageBins[]> {
-    const storageService = await connect.to("WAREHOUSESTORAGEBIN");
-    const { WarehouseStorageBin } = storageService.entities;
-    let storageBins: IStorageBins[];
-
-    if (req.query.SELECT?.where) {
-        storageBins = await storageService.run(SELECT.from(WarehouseStorageBin).where(req.query.SELECT.where));
-    } else {
-        storageBins = await storageService.run(SELECT.from(WarehouseStorageBin));
+    } catch (error) {
+        console.error("Error in getProducts: ", error);
+        throw new Error("Failed to fetch products");
     }
-
-    return storageBins;
 };
 
 
+const getVHStorageBins: OnEventHandler = async function (req: TypedRequest<{ EWMStorageBin_1: string }[]>): Promise<{ EWMStorageBin: string }[]> {
+    try {
+        const handlingCDS = await connect.to("YY1_HUINFOPALLETBOX");
+        const allStorageBins = await handlingCDS.run(SELECT.from('YY1_HUInfoPalletbox_ewm').columns('EWMStorageBin_1'));
+        let uniqueStorageBins: { EWMStorageBin: string }[] = [];
+
+        allStorageBins.forEach((item: { EWMStorageBin_1: string }) => {
+            if (item.EWMStorageBin_1 !== '' && !uniqueStorageBins.some(bin => bin.EWMStorageBin === item.EWMStorageBin_1)) {
+                uniqueStorageBins.push({ EWMStorageBin: item.EWMStorageBin_1 });
+            }
+        });
+
+        if ((req.query.SELECT as any)?.search) {
+            const filters = (req.query.SELECT as any).search as IWhereClause[];
+            const searchValues = filters.map(filter => filter.val as string);
+
+            uniqueStorageBins = uniqueStorageBins.filter((bin: { EWMStorageBin: string }) =>
+                searchValues.some(searchValue => bin.EWMStorageBin.includes(searchValue))
+            );
+        }
+
+        const finalUniqueStorageBins = Array.from(new Set(uniqueStorageBins.map(bin => bin.EWMStorageBin)))
+            .map(bin => ({ EWMStorageBin: bin }));
+
+        return finalUniqueStorageBins;
+
+    } catch (error) {
+        console.error("Error in getVHStorageBins: ", error);
+        throw new Error("Failed to fetch storage bins");
+    }
+};
+
+const getStorageTypes: OnEventHandler = async function (req: TypedRequest<{ EWMStorageType_1: string }[]>): Promise<{ EWMStorageType: string }[]> {
+    try {
+        const handlingCDS = await connect.to("YY1_HUINFOPALLETBOX");
+        const allStorageTypes = await handlingCDS.run(SELECT.from('YY1_HUInfoPalletbox_ewm').columns('EWMStorageType_1').where({
+            PackagingMaterialType: ['Z001', 'Z002']
+        }));
+        let uniqueStorageTypes: { EWMStorageType: string }[] = [];
+
+        allStorageTypes.forEach((item: { EWMStorageType_1: string }) => {
+            if (item.EWMStorageType_1 !== '' && !uniqueStorageTypes.some(type => type.EWMStorageType === item.EWMStorageType_1)) {
+                uniqueStorageTypes.push({ EWMStorageType: item.EWMStorageType_1 });
+            }
+        });
+
+        if ((req.query.SELECT as any)?.search) {
+            const filters = (req.query.SELECT as any).search as IWhereClause[];
+            const searchValues = filters.map(filter => filter.val as string);
+
+            uniqueStorageTypes = uniqueStorageTypes.filter((type: { EWMStorageType: string }) =>
+                searchValues.some(searchValue => type.EWMStorageType.includes(searchValue))
+            );
+        }
+
+        const finalUniqueStorageTypes = Array.from(new Set(uniqueStorageTypes.map(type => type.EWMStorageType)))
+            .map(type => ({ EWMStorageType: type }));
+
+        return finalUniqueStorageTypes;
+
+    } catch (error) {
+        console.error("Error in getStorageTypes: ", error);
+        throw new Error("Failed to fetch storage types");
+    }
+};
+
+
+const getProductionOrders: OnEventHandler = async function (req: TypedRequest<{ ProductionOrder: string }[]>): Promise<{ ProductionOrder: string }[]> {
+    try {
+        const handlingCDS = await connect.to("YY1_HUINFOPALLETBOX");
+        const allOrders = await handlingCDS.run(SELECT.from('YY1_HUInfoPalletbox_ewm').columns('ProductionOrder'));
+        let uniqueOrders: { ProductionOrder: string }[] = [];
+
+        allOrders.forEach((item: { ProductionOrder: string }) => {
+            if (item.ProductionOrder !== '' && !uniqueOrders.some(order => order.ProductionOrder === item.ProductionOrder)) {
+                uniqueOrders.push({ ProductionOrder: item.ProductionOrder });
+            }
+        });
+
+        if ((req.query.SELECT as any)?.search) {
+            const filters = (req.query.SELECT as any).search as IWhereClause[];
+            const searchValues = filters.map(filter => filter.val as string);
+
+            uniqueOrders = uniqueOrders.filter((order: { ProductionOrder: string }) =>
+                searchValues.some(searchValue => order.ProductionOrder.includes(searchValue))
+            );
+        }
+
+        const finalUniqueOrders = Array.from(new Set(uniqueOrders.map(order => order.ProductionOrder)))
+            .map(order => ({ ProductionOrder: order }));
+
+        return finalUniqueOrders;
+
+    } catch (error) {
+        console.error("Error in getProductionOrders: ", error);
+        throw new Error("Failed to fetch production orders");
+    }
+};
+
+const getEWMWarehouseBins: OnEventHandler = async function (req: TypedRequest<{ EWMWarehouse: string }>): Promise<IStorageBins[]> {
+    try {
+        const EWMWarehouse = req.data.EWMWarehouse;
+        const storageService = await connect.to("WAREHOUSESTORAGEBIN");
+        const { WarehouseStorageBin } = storageService.entities;
+        let storageBins: IStorageBins[];
+
+        storageBins = await storageService.run(SELECT.from(WarehouseStorageBin).where({ EWMWarehouse: EWMWarehouse }));
+
+        if ((req.query.SELECT as any)?.search) {
+            const filters = (req.query.SELECT as any).search as IWhereClause[];
+            const searchValues = filters.map(filter => filter.val as string);
+
+            storageBins = storageBins.filter(bin =>
+                searchValues.some(searchValue => bin.EWMStorageBin.includes(searchValue))
+            );
+        }
+
+        return storageBins;
+
+    } catch (error) {
+        console.error("Error in getEWMWarehouseBins: ", error);
+        throw new Error("Failed to fetch warehouse bins");
+    }
+};
+
+const getStorageBins: OnEventHandler = async function (req: TypedRequest<IStorageBins[]>): Promise<IStorageBins[]> {
+    try {
+        const storageService = await connect.to("WAREHOUSESTORAGEBIN");
+        const { WarehouseStorageBin } = storageService.entities;
+        let storageBins: IStorageBins[];
+
+        if (req.query.SELECT?.where) {
+            storageBins = await storageService.run(SELECT.from(WarehouseStorageBin).where(req.query.SELECT.where));
+        } else {
+            storageBins = await storageService.run(SELECT.from(WarehouseStorageBin));
+        }
+
+        if ((req.query.SELECT as any)?.search) {
+            const filters = (req.query.SELECT as any).search as IWhereClause[];
+            const searchValues = filters.map(filter => filter.val as string);
+
+            storageBins = storageBins.filter(bin =>
+                searchValues.some(searchValue => bin.EWMStorageBin.includes(searchValue))
+            );
+        }
+
+        return storageBins;
+
+    } catch (error) {
+        console.error("Error in getStorageBins: ", error);
+        throw new Error("Failed to fetch storage bins");
+    }
+};
 
 export {
     getHandlingUnits,
